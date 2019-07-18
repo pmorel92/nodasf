6a184b9674e13583ea4d9e09938222974fafdb5b
pmorel92 <philippe.a.b.morel@gmail.com>
2019-07-18 22:48:31 +0000

    models

--- nodasf/admin.py
+++ nodasf/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import County, Venue, Agency, Genre, Issue, City, Party, Level, Event, Politician, Media_Org, Journalist, Local_Link, STF, STF_Hub, STF_Link
+
+admin.site.register(County)
+admin.site.register(Venue)
+admin.site.register(Agency)
+admin.site.register(Genre)
+admin.site.register(Issue)
+admin.site.register(City)
+admin.site.register(Party)
+admin.site.register(Level)
+admin.site.register(Event)
+admin.site.register(Politician)
+admin.site.register(Media_Org)
+admin.site.register(Journalist)
+admin.site.register(Local_Link)
+admin.site.register(STF)
+admin.site.register(STF_Hub)
+admin.site.register(STF_Link)
