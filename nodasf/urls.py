0809e1f8bcfa51b52e36dfff6c3ea22d92ac767a
pmorel92 <philippe.a.b.morel@gmail.com>
2019-07-23 17:41:39 +0000

    slugigy

--- nodasf/urls.py
+++ nodasf/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^media-org-directory/$', views.media_directory, name='media org directory'),	
-	url(r'^media-org-directory/(?P<slug>[\w-]+)/$', views.media_org, name='media org'),	
+	url(r'^media-org/(?P<slug>[\w-]+)/$', views.media_org, name='media org'),	
 	url(r'^journalist/$', views.journalists, name='journalist directors'),
 	url(r'^venues/$', views.venues, name="venue directory"),
 	url(r'^politicians/$', views.politicians, name="politician directory"),
